499c643adea897234dff37ffd9b8b956fc96f33c
Breona <breona.caldwell98@gmail.com>
2018-07-19 16:16:33 -0500

    CG 2-1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + "!");
+            Console.ReadLine();        }
     }
 }
